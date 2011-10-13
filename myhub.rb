309c54de6af562d0b76bad3ca1b67c05c92aa9b9
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-10-12 21:51:43 -0500

    Fleshing out example to demonstrate github authorization

--- myhub.rb
+++ myhub.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'omniauth'
+
+use Rack::Session::Cookie
+
+use OmniAuth::Builder do
+  provider :github,   ENV['GITHUB_CLIENTID'], ENV['GITHUB_SECRET'] do |o|
+    o.authorize_params = {:scope => 'user,repo'}
+  end
+end
 
 get '/' do
-  "Hello, MyHub"
+  <<-HTML
+  <ul>
+    <li><a href='/auth/github'>Sign in with GitHub</a></li>
+  </ul>
+  HTML
+end
+
+get '/auth/:provider/callback' do
+  content_type 'text/plain'
+  request.env['omniauth.auth'].to_hash.inspect
 end
