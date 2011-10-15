17c1996a5a4ccd4470f9bfa3eaa3c754c99dd88b
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-10-14 21:35:52 -0500

    Require github login to access protected pages

--- myhub.rb
+++ myhub.rb
@@ -1,23 +1,62 @@
 require 'sinatra'
 require 'omniauth'
 
-use Rack::Session::Cookie
+PUBLIC_URLS = ['/', '/logout', '/auth/github', '/auth/github/callback']
+
+load '.settings' if File.exists? '.settings'
+if ENV['GITHUB_CLIENT_ID']
+  set :github_client_id, ENV['GITHUB_CLIENT_ID']
+  set :github_secret, ENV['GITHUB_SECRET']
+end
+
+enable :sessions
 
 use OmniAuth::Builder do
-  provider :github,   ENV['GITHUB_CLIENTID'], ENV['GITHUB_SECRET'] do |o|
+  provider :github,   settings.github_client_id, settings.github_secret do |o|
     o.authorize_params = {:scope => 'user,repo'}
+  end
+end
+
+before do
+  protected! unless PUBLIC_URLS.include? request.path_info
+end
+
+helpers do
+  def user_token
+    session["user_token"]
+  end
+
+  def logged_in
+    !!user_token
+  end
+
+  def protected!
+    redirect '/auth/github' unless logged_in 
   end
 end
 
 get '/' do
   <<-HTML
   <ul>
-    <li><a href='/auth/github'>Sign in with GitHub</a></li>
+    <li><a href='/github'>Explore GitHub</a></li>
   </ul>
   HTML
 end
 
-get '/auth/:provider/callback' do
-  content_type 'text/plain'
-  request.env['omniauth.auth'].to_hash.inspect
+get '/github' do
+  "Token: #{user_token}"
+end
+
+get '/auth/github/callback' do
+  omniauth = request.env['omniauth.auth']
+  session["user_token"] = omniauth['credentials']['token']
+  redirect '/github'
+
+  # content_type 'text/plain'
+  # request.env['omniauth.auth'].to_hash.inspect
+end
+
+get '/logout' do
+  session["user_token"] = nil
+  redirect '/'
 end
