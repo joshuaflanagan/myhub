8de3d8d9866fcb3ad42323056b760a9df1a22949
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-10-19 21:54:01 -0500

    Show repos/teams/members for a selected org

--- public/js/myhub.js
+++ public/js/myhub.js
@@ -1,17 +1,49 @@
 var github = function(resource, callback){
-  $.getJSON(github_url + resource, {access_token: token}, callback);
+  var github_url = 'https://api.github.com';
+  var url = resource.slice(0, 4) == 'http' ? resource : github_url + resource;
+  return $.getJSON(url, {access_token: token}, callback);
 };
 
-var org_list = '{{#orgs}}<li><a href="{{url}}"><img src="{{avatar_url}}">{{login}}</a></li>{{/orgs}}';
+var org_list = '<h3>Organizations</h3><ul>{{#orgs}}<li><a href="{{url}}"><img src="{{avatar_url}}">{{login}}</a></li>{{/orgs}}</ul>';
+var org_display = '<h3>{{org_name}}</h3>' +
+  '<ul>{{#repos}}<li>{{name}}</li>{{/repos}}</ul>' +
+  '<ul>{{#teams}}<li>{{name}}</li>{{/teams}}</ul>' +
+  '<ul>{{#members}}<li>{{login}}</li>{{/members}}</ul>';
 
 $(function(){
-  var orgs = $('#organizations');
   github('/user/orgs', function(data){
-    var html = Mustache.to_html(org_list, {orgs: data});
-    orgs.append(html);
-    $("a", orgs).click(function(e){
-      e.preventDefault();
-      var orgUrl = $(this).attr('href');
-      console.log('clicked ' + this); });
+    show_orgs(data);
   });
 });
+
+var show_orgs = function(data){
+  var page_holder = $('#page-holder');
+  var html = Mustache.to_html(org_list, {orgs: data});
+  page_holder.html(html);
+  $("a", page_holder).click(function(e){
+    e.preventDefault();
+    var orgUrl = $(this).attr('href');
+    var orgName = $(this).text();
+    select_org({name: orgName, url: orgUrl});
+  });
+};
+
+var select_org = function(org){
+  $.when( github(org.url + '/repos'),
+          github(org.url + '/teams'),
+          github(org.url + '/members')
+        ).done(function(repoArgs, teamArgs, memberArgs){
+          show_org_details(org, repoArgs[0], teamArgs[0], memberArgs[0]);
+        });
+};
+
+var show_org_details = function(org, repos, teams, members){
+  var page_holder = $('#page-holder');
+  var html = Mustache.to_html(org_display, {
+    org_name: org.name,
+    repos: repos,
+    teams: teams,
+    members: members
+  });
+  page_holder.html(html);
+};
