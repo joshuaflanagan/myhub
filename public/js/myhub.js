a863f4e255a7054c36fb51c08faa43ee1f6a2ae1
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-10-23 14:14:27 -0500

    Move app logic out of global functions and into a MyHub class

--- public/js/myhub.js
+++ public/js/myhub.js
@@ -1,59 +1,74 @@
-var github = function(resource, callback){
-  var github_url = 'https://api.github.com';
-  var url = resource.slice(0, 4) == 'http' ? resource : github_url + resource;
-  var headers = {
-    "Authorization" : "token " + token
+(function($){
+
+// This is the main application logic
+//  authenticatedUser: Should have a token property with
+//                     a valid GitHub OAuth access token.
+//  page: css selector for the DOM element used to render the app
+//
+var myHub = function(authenticatedUser, page){
+  var self = this;
+  page = $(page);
+
+  var templ = function(view){
+    return $("#view-" + view).html();
   };
-  return $.ajax({
-    url: url,
-    dataType: "json",
-    success: callback,
-    headers: headers
-  });
+
+  var bind_templ = function(view, model){
+    return Mustache.to_html(templ(view), model);
+  };
+
+  var github = function(resource, callback){
+    var github_url = 'https://api.github.com';
+    var url = resource.slice(0, 4) == 'http' ? resource : github_url + resource;
+    var headers = {
+      "Authorization" : "token " + authenticatedUser.token
+    };
+    return $.ajax({
+      url: url,
+      dataType: "json",
+      success: callback,
+      headers: headers
+    });
+  };
+
+  this.manageOrgs = function(){
+    github('/user/orgs', function(data){
+      self.show_orgs(data);
+    });
+  };
+
+  this.show_orgs = function(data){
+    var html = bind_templ("org-list", {orgs: data});
+    page.html(html);
+    $("a", page).click(function(e){
+      e.preventDefault();
+      var orgUrl = $(this).attr('href');
+      var orgName = $(this).text();
+      self.select_org({name: orgName, url: orgUrl});
+    });
+  };
+
+  this.select_org = function(org){
+    $.when( github(org.url + '/repos'),
+           github(org.url + '/teams'),
+           github(org.url + '/members')
+          ).done(function(repoArgs, teamArgs, memberArgs){
+            self.show_org_details(org, repoArgs[0], teamArgs[0], memberArgs[0]);
+          });
+  };
+
+  this.show_org_details = function(org, repos, teams, members){
+    var html = bind_templ("org", {
+      org_name: org.name,
+      repos: repos,
+      teams: teams,
+      members: members
+    });
+    page.html(html);
+  };
+
 };
 
-$(function(){
-  github('/user/orgs', function(data){
-    show_orgs(data);
-  });
-});
+window.MyHub = myHub;
 
-var templ = function(view){
-  return $("#view-" + view).html();
-};
-
-var bind_templ = function(view, model){
-  return Mustache.to_html(templ(view), model);
-};
-
-var show_orgs = function(data){
-  var page_holder = $('#page-holder');
-  var html = bind_templ("org-list", {orgs: data});
-  page_holder.html(html);
-  $("a", page_holder).click(function(e){
-    e.preventDefault();
-    var orgUrl = $(this).attr('href');
-    var orgName = $(this).text();
-    select_org({name: orgName, url: orgUrl});
-  });
-};
-
-var select_org = function(org){
-  $.when( github(org.url + '/repos'),
-          github(org.url + '/teams'),
-          github(org.url + '/members')
-        ).done(function(repoArgs, teamArgs, memberArgs){
-          show_org_details(org, repoArgs[0], teamArgs[0], memberArgs[0]);
-        });
-};
-
-var show_org_details = function(org, repos, teams, members){
-  var page_holder = $('#page-holder');
-  var html = bind_templ("org", {
-    org_name: org.name,
-    repos: repos,
-    teams: teams,
-    members: members
-  });
-  page_holder.html(html);
-};
+})(jQuery);
