ec25200147f3e1ea4b48ad63488adf8f77bb010a
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-10-23 19:09:38 -0500

    Add Manage User page

--- public/js/myhub.js
+++ public/js/myhub.js
@@ -10,9 +10,10 @@ var myHub = function(authenticatedUser, page){
   page = $(page);
   this.el = page;
   this.org = null;
+  this.managedUser = null;
 
   var templ = function(view){
-    return $("#view-" + view).html();
+    return $('#view-' + view).html();
   };
 
   var bind_templ = function(view, model){
@@ -23,11 +24,11 @@ var myHub = function(authenticatedUser, page){
     var github_url = 'https://api.github.com';
     var url = resource.slice(0, 4) == 'http' ? resource : github_url + resource;
     var headers = {
-      "Authorization" : "token " + authenticatedUser.token
+      'Authorization' : 'token ' + authenticatedUser.token
     };
     return $.ajax({
       url: url,
-      dataType: "json",
+      dataType: 'json',
       success: callback,
       headers: headers
     });
@@ -39,38 +40,66 @@ var myHub = function(authenticatedUser, page){
     });
   };
 
-  page.bind("org-selected", function(evt, orgLogin, orgUrl){
-    self.org = {name: orgLogin, url: orgUrl};
-    self.load_org(self.org);
+  page.bind('org-selected', function(evt, orgLogin, orgUrl){
+    self.org = {login: orgLogin, url: orgUrl};
+    var loading_org = self.load_org(self.org);
+    loading_org.done(function(){
+      self.show_user_selection();
+    });
+  });
+
+  page.bind('user-selected', function(evt, userLogin, userUrl){
+    self.managedUser = {login: userLogin, url: userUrl};
+    var loading_user = self.load_user(self.managedUser);
+    loading_user.done(function(){
+      self.show_manage_user();
+    });
   });
 
   this.show_orgs = function(data){
-    var html = bind_templ("org-list", {orgs: data});
+    var html = bind_templ('org-list', {orgs: data});
     page.html(html);
-    $("a", page).click(function(e){
+    $('a', page).click(function(e){
       e.preventDefault();
       var orgUrl = $(this).attr('href');
       var orgLogin = $(this).text();
-      page.trigger("org-selected", [orgLogin, orgUrl]);
+      page.trigger('org-selected', [orgLogin, orgUrl]);
     });
   };
 
   this.load_org = function(org){
-    $.when( github(org.url + '/repos'),
+    return $.when( github(org.url + '/repos'),
            github(org.url + '/teams'),
            github(org.url + '/members')
           ).done(function(repoArgs, teamArgs, memberArgs){
-            self.show_org_details(org, repoArgs[0], teamArgs[0], memberArgs[0]);
+            org.repos = repoArgs[0];
+            org.teams = teamArgs[0];
+            org.members = memberArgs[0];
           });
   };
 
-  this.show_org_details = function(org, repos, teams, members){
-    var html = bind_templ("org", {
-      org_name: org.name,
-      repos: repos,
-      teams: teams,
-      members: members
+  this.load_user = function(user){
+    return $.when(user);
+  };
+
+  this.show_user_selection = function(){
+    var html = bind_templ('user-selection', self.org);
+    page.html(html);
+    $('a', page).click(function(e){
+      e.preventDefault();
+      var userUrl = $(this).attr('href');
+      var userLogin = $(this).text();
+      page.trigger('user-selected', [userLogin, userUrl]);
     });
+  };
+
+  this.show_manage_user = function(){
+    var page_data = {
+      user_login: self.managedUser.login,
+      org_login: self.org.login,
+      org_repos: self.org.repos
+    };
+    var html = bind_templ('manage-user', page_data);
     page.html(html);
   };
 
